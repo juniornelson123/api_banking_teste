88d38a0542dc83090ed4731a4860963352e4ab17
junior nelson <junior123nelson@gmail.com>
2020-03-02 22:36:30 -0300

    fix:web command docker

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,14 +4,24 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url = System.get_env("DATABASE_URL")
+database_url = System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
 
 config :api_banking, ApiBanking.Repo,
-  # ssl: true,
+  ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  database: "your_db_name_here"
 
-secret_key_base = System.get_env("SECRET_KEY_BASE")
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
 
 config :api_banking, ApiBankingWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
