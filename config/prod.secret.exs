099dc944d9a55e9fbf0f632e1accd6309c51427b
junior nelson <junior123nelson@gmail.com>
2020-03-02 22:18:04 -0300

    fix:web command docker

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -11,12 +11,7 @@ config :api_banking, ApiBanking.Repo,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+secret_key_base = System.get_env("SECRET_KEY_BASE")
 
 config :api_banking, ApiBankingWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
