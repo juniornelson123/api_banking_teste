d382f0925f6076dafcfd1d7b1b162e998949416a
junior nelson <junior123nelson@gmail.com>
2020-03-03 00:00:47 -0300

    fix: alter version elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ApiBanking.MixProject do
     [
       app: :api_banking,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
